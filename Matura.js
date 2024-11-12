54d5498e78ef5ab8ac6e53ba4f3650accbb613e5
Lequest1 <brg.maturaball@gmail.com>
2024-11-12 21:16:43 +0100

    Lorem ipsum spam + task bar

--- Matura.js
+++ Matura.js
@@ -1,6 +1,7 @@
+var num = 3
 function warning(){
-	document.getElementById("hinter").innerHTML=document.getElementById("hinter").innerHTML + "<div id='alert' class='alert'><p class='n_text'>Diese Website ist noch nicht vollendet, weswegen jeglicher Inhalt weder von dem Maturaball Komitee befürwortet wird noch ihrer Meinung entspricht</p>"+
-	"<br><br><p class='n_text'> Schreibe "+'"'+"Ich habe verstanden"+'"'+ " um dieses Fenster zu schließen</p><br>"+
+	document.getElementById("hinter").innerHTML=document.getElementById("hinter").innerHTML + "<div id='alert' class='alert'><p class='f_text'>Diese Website ist noch nicht vollendet, weswegen jeglicher Inhalt weder von dem Maturaball Komitee befürwortet wird noch ihrer Meinung entspricht</p>"+
+	"<br><br><p class='f_text'> Schreibe "+'"'+"Ich habe verstanden"+'"'+ " um dieses Fenster zu schließen</p><br>"+
 	"<input type='text' id='verstanden' class=''><br><br><input type='submit'value='Fertig' onclick='check_v()'></div> <div class='hintergrund' id='hintergrund'></div>"
 }
 function check_v(){
@@ -15,8 +16,8 @@ function check_v(){
 	}
 }
 
-function test(){
-if (window.innerWidth>=window.innerHeight){
+function resize(){
+if (window.innerWidth>=window.innerHeight){ //check for phone
 $(".follow").css({
  	top:"0px",
 	position:"absolute",
@@ -32,6 +33,18 @@ $(".follow").css({
 	height:"100vw",
   });
 }
+  $(".tabs").css({
+  "font-size":"20px",
+  width: ($("#tabs_SCP").innerWidth()-4*num)/num+"px",
+  "min-height":"22px",
+   border: "2px black solid",
+  display: "inline-block",
+overflow:"none"
+  });
 }
  setTimeout(warning, 1)
- setInterval(test,1)
+ setInterval(resize,1)
+function skip(){
+document.getElementById("alert").remove()
+		document.getElementById("hintergrund").remove()
+}
